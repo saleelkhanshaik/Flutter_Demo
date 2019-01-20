e468693c6d0eaa246a854207915f07cae717e7da
saleelkhanshaik <saleelkhan03.shaik@gmail.com>
2019-01-20 21:01:25 +0530

    1.Added the font files in fonts package and modified the pubSpac.yaml (called the imported fonts) and worked on the text style ,Worked on Row,column,Expendad widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,13 @@
 import 'package:flutter/material.dart';
-import './app_screens/first_screen.dart';
+import 'app_screens/Home.dart';
 
-/*=> function expression insteaded of {}
-void main()
-{
-  runApp(new MyFlutterDemo());
-}*/
-void main() => runApp(new MyFlutterDemo());
-
-class MyFlutterDemo extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return MaterialApp(
-        //to remove the debug badge in the top
-        debugShowCheckedModeBanner: false,
-        title: "My Flutter App",
-        home: Scaffold(
-          //allowing to app bar on the top of the screen
-          appBar: AppBar(title: Text("HOME"),
-          ),
-          body: new FiestScreen()
-        )
-    );
-  }
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "exploring the Widgets",
+    home: Home()
+  ),
+  );
 }
+
+
