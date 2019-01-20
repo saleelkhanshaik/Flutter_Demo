f2109432d8b7d6c2435791319538683b6e9cbcc4
saleelkhanshaik <saleelkhan03.shaik@gmail.com>
2019-01-20 14:51:43 +0530

    Worked on Scffold to make app bar and text with center alienment wirh style

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,27 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    Center(child:Text(
-        "hello world",
-        textAlign: TextAlign.center,
-        textDirection: TextDirection.ltr
+    MaterialApp(
+      title: "My Flutter App",
+      home:Scaffold(//allowing to app bar on the top of the screen
+        appBar: AppBar(title: Text("HOME"),),
+        body: Material(
+          color: Colors.lightBlue,
+          child:
+          Center(child:Text(
+            "hello world",
+            textAlign: TextAlign.center,
+            textDirection: TextDirection.ltr,
+            style: TextStyle(color: Colors.white,fontSize: 40.0,
+                fontStyle: FontStyle.italic),
+          ),
+          ),
+        ),
+
+      )
+
     )
-      ,)
+
 
   );
 
